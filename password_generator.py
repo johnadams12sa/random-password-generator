45b2fe99db28c4312ec2db1fcebc474827fb3876
Aaron Yam <ayam5005@gmail.com>
2022-01-12 15:25:20 -0500

    separated program into functions

--- password_generator.py
+++ password_generator.py
@@ -1,60 +1,74 @@
-#Random Password Generator App
+#Random Password Generator App v2.0
 #written by Aaron Yam
 
 import string
 import random
 import pyperclip
 
-lowercase = list(string.ascii_lowercase)
-uppercase = list(string.ascii_uppercase)
-digits = list(string.digits)
-special_characters = list(string.punctuation)
-special_characters_basic = list('?!@#$%^&*()_+-=')
+def userInput():
+    try:
+        passwordLength = eval(input('How many characters are in the password? '))
+    except SyntaxError:
+        print("Invalid input, non-integer value was entered\n")
+        exit()
+    except NameError:
+        print("Invalid input, non-integer value was entered\n")
+        exit()
 
-try:
-    passwordLength = eval(input('How many characters are in the password? '))
-except SyntaxError:
-    print("Invalid input, non-integer value was entered\n")
-    exit()
-except NameError:
-    print("Invalid input, non-integer value was entered\n")
-    exit()
-
-try:
-    passwordOptions = input('What options would you like to include? \n\n \
-Lowercase    (abc)       (L) \n \
-Uppercase    (ABC)       (U) \n \
-Number       (123)       (N) \n \
-Symbol       (!@#$)      (S) \n \
-Punctuation  (,.{[\\]})   (A) \n \
-SELECT ALL               (Z) \n\n\
+    try:
+        passwordOptions = input('What options would you like to include? \n\n \
+    Lowercase    (abc)       (L) \n \
+    Uppercase    (ABC)       (U) \n \
+    Number       (123)       (N) \n \
+    Symbol       (!@#$)      (S) \n \
+    Punctuation  (,.{[\\]})   (A) \n \
+    SELECT ALL               (Z) \n\n\
 Choice(s): ')
-    if not passwordOptions:
-        raise ValueError("No choices were selected\n")
-except ValueError as e:
-    print(e)
+        if not passwordOptions:
+            raise ValueError("No choices were selected\n")
+    except ValueError as e:
+        print(e)
 
-passwordOptions = passwordOptions.replace(" ","").upper()
-enabledOptions = list(passwordOptions)
-lists = []
-if enabledOptions == []:
-    exit()
-else:
-    if 'Z' in enabledOptions:
-        lists.extend((lowercase, uppercase, digits, special_characters_basic, special_characters))
+    passwordOptions = passwordOptions.replace(" ","").upper()
+    enabledOptions = list(passwordOptions)
+    userPreference = [passwordLength,enabledOptions]
+    return userPreference
+
+def generatePassword(userInputValues):
+    lowercase = list(string.ascii_lowercase)
+    uppercase = list(string.ascii_uppercase)
+    digits = list(string.digits)
+    special_characters = list(string.punctuation)
+    special_characters_basic = list('?!@#$%^&*()_+-=')
+
+    lists = []
+    if userInputValues[1] == []:
+        exit()
     else:
-        if 'L' in enabledOptions: lists.append(lowercase)
-        if 'U' in enabledOptions: lists.append(uppercase)
-        if 'N' in enabledOptions: lists.append(digits)
-        if 'S' in enabledOptions: lists.append(special_characters_basic)
-        if 'A' in enabledOptions: lists.append(special_characters)
+        if 'Z' in userInputValues[1]:
+            lists.extend((lowercase, uppercase, digits, special_characters_basic, special_characters))
+        else:
+            if 'L' in userInputValues[1]: lists.append(lowercase)
+            if 'U' in userInputValues[1]: lists.append(uppercase)
+            if 'N' in userInputValues[1]: lists.append(digits)
+            if 'S' in userInputValues[1]: lists.append(special_characters_basic)
+            if 'A' in userInputValues[1]: lists.append(special_characters)
 
-password = ""
+    password = ""
 
-for characters in range(passwordLength):
-    characters = random.choice(random.choice(lists))
-    password = password + characters
+    for characters in range(userInputValues[0]):
+        characters = random.choice(random.choice(lists))
+        password = password + characters
 
-print(password)
-pyperclip.copy(password)
-print("Password has been copied to your clipboard") 
+    print(password)
+    pyperclip.copy(password)
+    print("Password has been copied to your clipboard")
+    return
+
+def main():
+    userInputValues = userInput()
+    generatePassword(userInputValues)
+    return
+
+if __name__ == "__main__":
+    main()
